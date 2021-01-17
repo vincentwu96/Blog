98bd6674dda51ac71ae7c7af8c5c591db104885c
vincentwu96 <vincentwu96@yahoo.com>
2021-01-16 19:00:53 -0800

    front end for GET call

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Post from './Post/Post';
 import useStyles from './styles';
-
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 const Posts = () => {
@@ -11,11 +11,15 @@ const Posts = () => {
     console.log(posts);
 
     return (
-        <>
-        <h1>POSTS</h1>
-        <Post />
-        <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
